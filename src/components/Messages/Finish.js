2a7642e647ccbcb8efc920b59394ade6199557d3
Dana <dana.ottaviani@gmail.com>
2020-07-04 10:18:08 -0400

    Display level and moves on finish page

--- src/components/Messages/Finish.js
+++ src/components/Messages/Finish.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 import {connect} from 'react-redux';
 
+import Button from '../Button/Button';
+
 const Finish = props => {
     return (
         <div>
-            <h1>You solved it!</h1>
-            <button>Play again</button>
+            <h1>You solved level {props.level} in {props.moves} moves!</h1>
+            <Button class="PlayAgain" text="Play Again" />
         </div>
     );
 };
+
+const mapStateToProps = state => {
+    return {
+        level: state.level,
+        moves: state.moves
+    }
+}
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -17,4 +26,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null,mapDispatchToProps)(Finish);
+export default connect(mapStateToProps,mapDispatchToProps)(Finish);
