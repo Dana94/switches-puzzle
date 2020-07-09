390e9194e44caf0f8972cc8d501f4ec55f09b823
Dana <dana.ottaviani@gmail.com>
2020-07-09 18:15:04 -0400

    fix: remove switch focus if reset is clicked

--- src/components/Messages/Welcome/Welcome.js
+++ src/components/Messages/Welcome/Welcome.js
@@ -3,21 +3,19 @@ import { connect } from 'react-redux';
 
 import { reset, setLevel } from '../../../store/actions/switches';
 
-import classes from './Welcome.module.css';
+import './Welcome.module.css';
 import Button from '../../Button/Button';
 
 const Welcome = props => {
 
     return (
         <div>
-            <div className={classes.Container}>
-                <h1>Switches Puzzle</h1>
-                <p>Choose a Level:</p>
-                <div>
-                    <Button click={() => props.onSetLevel(1)} class="Green" text="Level 1" />
-                    <Button click={() => props.onSetLevel(2)} class="Red" text="Level 2" />
-                    <Button click={() => props.onSetLevel(3)} class="Black" text="Level 3" />
-                </div>
+            <h1>Switches Puzzle</h1>
+            <h2>Choose a Level:</h2>
+            <div>
+                <Button click={() => props.onSetLevel(1)} class="Green" text="Level 1" />
+                <Button click={() => props.onSetLevel(2)} class="Red" text="Level 2" />
+                <Button click={() => props.onSetLevel(3)} class="Black" text="Level 3" />
             </div>
         </div>
     );
--- src/components/Switches/Switches.js
+++ src/components/Switches/Switches.js
@@ -4,23 +4,33 @@ import { connect } from 'react-redux';
 import './Switches.css';
 import Row from './Row/Row';
 import Button from '../Button/Button';
-import { reset, endGame } from '../../store/actions/switches';
+import { reset, endGame, setFocus } from '../../store/actions/switches';
 
 const Switches = props => {
 
+    const onResetHandler = () => {
+        props.onReset();
+        props.onSetFocus({ x: null, y: null });
+
+    }
+
     return (
         <div>
-            <p>Turn all the switches on so their color is green.</p>
-            <div className="Container">
+            <p className="Instructions">Turn all the switches on so their color is green.</p>
+            <div>
+                <Button click={onResetHandler} class="Black" text="Reset" />
+                <Button click={() => props.onEndGame()} class="Green" text="End Game" />
+                <p className="Moves">Moves: {props.moves}</p>
+            </div>
+            <div>
                 {
                     props.switches.map((row, index) => {
                         return <Row x={index} key={index} row={row} />;
                     })
                 }
             </div>
-            <Button click={() => props.onReset()} class="Black" text="Reset" />
-            <Button click={() => props.onEndGame()} class="Green" text="End Game" />
-            <p>Moves: {props.moves}</p>
+
+
         </div>
     )
 }
@@ -36,7 +46,9 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         onReset: () => dispatch(reset()),
-        onEndGame: () => dispatch(endGame())
+        onEndGame: () => dispatch(endGame()),
+        onSetFocus: (coords) => dispatch(setFocus(coords))
+
     }
 }
 
